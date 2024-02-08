2c55e6f75f80fc4999110fdbb57631c4a15d1c90
Trond Thorsteinsson <trtho1068@oslomet.no>
2024-02-08 15:36:44 +0100

    Different error messages for input elems depending on validityState, only differentiate missingValue and whatever else. New InputValidator objects to handle input events and errorMessages, mainly for the sake of learning a little on objects and prototypes. Reset error messages when form is reset. Some minimal styling for less vomiting.

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,43 +1,66 @@
-let form = document.querySelector("#ticket-order");
+"use strict";
 
-let tickets = JSON.parse(sessionStorage.getItem("tickets"));
-tickets ||= [];  // if false (null), empty array
+/*
+* Not necessary to solve the assignment, but an opportunity
+* for me to learn a little about js objects and prototypes
+*/
+function InputValidator(inputElem, errorElem, missingValueMsg, badValueMsg) {
+    this.inputElem = inputElem;
+    this.errorElem = errorElem;
+    this.missingValueMsg = missingValueMsg;
+    this.badValueMsg = badValueMsg;
+}
 
-let inputNumber = document.querySelector("#number");
-let inputFirstName = document.querySelector("#firstname");
-let inputLastName = document.querySelector("#lastname");
-let inputPhoneNumber = document.querySelector("#phone-number");
-let inputEmail = document.querySelector("#email");
+InputValidator.prototype.register = function (eventType) {
+    this.inputElem.addEventListener(eventType, this);
+};
 
-let errorNumber = document.querySelector("#error-number")
-let errorFirstName = document.querySelector("#error-firstname");
-let errorLastName = document.querySelector("#error-lastname");
-let errorPhoneNumber = document.querySelector("#error-phone-number");
-let errorEmail = document.querySelector("#error-email");
+// Enable registering instances as event handlers
+InputValidator.prototype.handleEvent = function() {
+    this.validate();
+}
 
-let buttonBuyTickets = document.querySelector("#buy-tickets");
-let buttonDeleteTickets = document.querySelector("#delete-tickets");
+/*
+ * Can not be used directly with addEventListener as "this" in that
+ * context would refer to the event target i.e. inputElem --> TypeError
+ * Some options:
+ * 1) Implement the required interface (handleEvent)
+ * 2) Register an anonymous func calling validate
+ */
+InputValidator.prototype.validate = function () {
+    let validityState = this.inputElem.validity;
+    console.log(validityState);
 
-let tBodyAllTickets = document.querySelector("#all-tickets");
-for (let ticket of tickets) {
-    insertTableRow(tBodyAllTickets, ticket);
+    if (validityState.valid) {
+        if (this.errorElem.textContent) {
+            this.errorElem.textContent = "";
+        }
+    } else {
+        this.showErrorMessage(validityState);
+    }
+};
+
+InputValidator.prototype.showErrorMessage = function (validityState) {
+    if (validityState.valueMissing) {
+        this.errorElem.textContent = this.missingValueMsg;
+    } else {
+        this.errorElem.textContent = this.badValueMsg;
+    }
+};
+
+
+function Ticket(movie, number, firstName, lastName, phoneNumber, email) {
+    this.movie = movie;
+    this.number = number;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
 }
 
 
-function Ticket(formData) {
-    return {
-        movie: formData.get("movie"),
-        number: parseInt(formData.get("number")),
-        firstname: formData.get("firstname"),
-        lastname: formData.get("lastname"),
-        phoneNumber: formData.get("phone-number"),
-        email: formData.get("email"),
-    };
-}
-
-
-function insertTableRow(tElement, ticket) {
-    let newRow = tElement.insertRow();
+function insertIntoTableElement(ticket, tableElement) {
+    let newRow = tableElement.insertRow();
     for (let key in ticket) {
         let cell = newRow.insertCell();
         cell.textContent = ticket[key];
@@ -45,69 +68,126 @@ function insertTableRow(tElement, ticket) {
 }
 
 
-inputNumber.addEventListener("input", (event) => {
-    if (inputNumber.value && !inputNumber.checkValidity()) {
-        errorNumber.textContent = "Ugyldig antall"
-    } else {
-        errorNumber.textContent = "";
-    }
+/******************************************************************************/
+let tickets = [];
+let form = document.querySelector("#ticket");
+let movies = [
+    "Into the Wild",
+    "Børning 1",
+    "Børning 2",
+    "Trolljegeren",
+];
+
+let movieValidator = new InputValidator(
+    document.querySelector("#movie"),
+    document.querySelector("#error-movie"),
+    "Må velge film"
+)
+let numberValidator = new InputValidator(
+    document.querySelector("#number"),
+    document.querySelector("#error-number"),
+    "Antall må fylles ut",
+    "Ugyldig antall"
+);
+let firstNameValidator = new InputValidator(
+    document.querySelector("#firstname"),
+    document.querySelector("#error-firstname"),
+    "Fornavn må fylles ut",
+    "Ugyldig fornavn"
+);
+let lastNameValidator = new InputValidator(
+    document.querySelector("#lastname"),
+    document.querySelector("#error-lastname"),
+    "Etternavn må fylles ut",
+    "Ugyldig etternavn"
+);
+let phoneNumberValidator = new InputValidator(
+    document.querySelector("#phone-number"),
+    document.querySelector("#error-phone-number"),
+    "",
+    "Ugyldig telefonnr"
+);
+let emailValidator = new InputValidator(
+    document.querySelector("#email"),
+    document.querySelector("#error-email"),
+    "Epost addresse må fylles ut",
+    "Ugyldig epost addresse"
+);
+
+let buttonBuyTicket = document.querySelector("#buy-ticket");
+let buttonDeleteTickets = document.querySelector("#delete-tickets");
+let tableBodyAllTickets = document.querySelector("#all-tickets");
+
+
+if (sessionStorage.getItem("tickets")) {
+    tickets = JSON.parse(sessionStorage.getItem("tickets"));
+}
+
+for (let ticket of tickets) {
+    insertIntoTableElement(ticket, tableBodyAllTickets);
+}
+
+for (let movie of movies) {
+    let option = document.createElement("option");
+    option.value = movie;
+    option.textContent = movie;
+    movieValidator.inputElem.add(option);
+}
+
+if (sessionStorage.getItem("selectedIndex")) {
+    movieValidator.inputElem.selectedIndex =
+        sessionStorage.getItem("selectedIndex");
+}
+
+let initErrorMessages = () => {
+    movieValidator.validate();
+    numberValidator.validate();
+    firstNameValidator.validate();
+    lastNameValidator.validate();
+    phoneNumberValidator.validate();
+    emailValidator.validate();
+};
+initErrorMessages();
+
+movieValidator.register("change");
+movieValidator.inputElem.addEventListener("change", (event) => {
+   sessionStorage.setItem(
+       "selectedIndex", movieValidator.inputElem.selectedIndex
+   )
 });
+numberValidator.register("input");
+firstNameValidator.register("input");
+lastNameValidator.register("input");
+phoneNumberValidator.register("input");
+emailValidator.register("input");
 
-
-inputFirstName.addEventListener("input", (event) => {
-    if (inputFirstName.value && !inputFirstName.checkValidity()) {
-        errorFirstName.textContent = "Ugyldig fornavn";
-    } else {
-        errorFirstName.textContent = "";
-    }
-});
-
-
-inputLastName.addEventListener("input", (event) => {
-    if (inputLastName.value && !inputLastName.checkValidity()) {
-        errorLastName.textContent = "Ugyldig etternavn";
-    } else {
-        errorLastName.textContent = "";
-    }
-});
-
-
-inputPhoneNumber.addEventListener("input", (event) => {
-    if (inputPhoneNumber.value && !inputPhoneNumber.checkValidity()) {
-        errorPhoneNumber.textContent = "Ugyldig telefonnr";
-    } else {
-        errorPhoneNumber.textContent = "";
-    }
-});
-
-
-inputEmail.addEventListener("input", (event) => {
-    if (inputEmail.value && !inputEmail.checkValidity()) {
-        errorEmail.textContent = "Ugyldig epost addresse";
-    } else {
-        errorEmail.textContent = "";
-    }
-});
-
-
-buttonBuyTickets.addEventListener("click", (event) => {
+buttonBuyTicket.addEventListener("click", (event) => {
     if (form.checkValidity()) {
-        let ticket = new Ticket(new FormData(form));
+        let ticket = new Ticket(
+            movieValidator.inputElem.value,
+            parseInt(numberValidator.inputElem.value),
+            firstNameValidator.inputElem.value,
+            lastNameValidator.inputElem.value,
+            phoneNumberValidator.inputElem.value,
+            emailValidator.inputElem.value,
+        )
         tickets.push(ticket);
-        form.reset();
+
+        insertIntoTableElement(ticket, tableBodyAllTickets);
         sessionStorage.setItem("tickets", JSON.stringify(tickets));
-        insertTableRow(tBodyAllTickets, ticket);
+        sessionStorage.removeItem("selectedIndex");
+
+        form.reset();
+        initErrorMessages();
     }
 });
 
 buttonDeleteTickets.addEventListener("click", (event) => {
-    // Are you sure?...
-    // Surprised to learn that there is no method array.clear
     tickets.splice(0, tickets.length);
     sessionStorage.removeItem("tickets");
 
-    while (tBodyAllTickets.firstChild) {
+    while (tableBodyAllTickets.firstChild) {
         // firstChild null if no children
-        tBodyAllTickets.removeChild(tBodyAllTickets.firstChild);
+        tableBodyAllTickets.removeChild(tableBodyAllTickets.firstChild);
     }
 });
